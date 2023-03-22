6149be200dda8e159cd6e1e779905e317c2b47a5
wonchaeYi <wonchae.yi@softpkorea.com>
2023-03-22 15:18:58 +0900

    file첨부 진행중

--- Board/Controllers/NoticesController.cs
+++ Board/Controllers/NoticesController.cs
@@ -40,7 +40,9 @@ namespace Board.Controllers
         public List<Comments> Comments { get; set; }
         public string Category { get; set; }
 
-        
+        public string? FileName { get; set; }
+        public string? fileAttachMent { get; set; }
+
     }
 
 
@@ -56,7 +58,6 @@ namespace Board.Controllers
         }
 
 
-
         // GET: Notices
         [ActionName("Index")]
         public async Task<IActionResult> Index(string? Category, string? searchString)
@@ -64,26 +65,29 @@ namespace Board.Controllers
 
             //LINQ to get list of category
             IQueryable<string> categoryQuery = from m in _context.Notice
+                                               orderby m.UpdateDate descending
                                                select m.Category;
 
             var notice = from m in _context.Notice select m;
-           
+            notice = from m in _context.Notice orderby m.UpdateDate descending select m;
 
             if (!string.IsNullOrEmpty(searchString)) {
-                notice = notice.Where(x => x.Title!.Contains(searchString));
+
+                notice = notice.Where(x => x.Title!.Contains(searchString)); 
             }
 
             if (!string.IsNullOrEmpty(Category))
             {
+               
                 notice = notice.Where(x => x.Category == Category);
               
             }
+
             var noticeCategory = new NoticeCategory
             {
                 Categorys = new SelectList(await categoryQuery.Distinct().ToListAsync()),
                 Notices = await notice.ToListAsync()
                 
-
             };
            
             //var notice_list = Enumerable.Reverse(_context.Notice).ToList();
@@ -95,7 +99,6 @@ namespace Board.Controllers
         public async Task<IActionResult> Details(int? id, [Bind("Id,Comment,UserName")] Notice notice)
         {
 
-
             if (id == null || _context.Notice == null)
             {
                 return NotFound();
@@ -105,13 +108,9 @@ namespace Board.Controllers
                 notice.Views_Number++;
                 await _context.SaveChangesAsync();
             
-            //var comment = from m in _context.Comments select m;
-            //comment = comment.Where(s => s.Notice_id == id);
 
 
             var comment = from m in _context.Comments where m.Notice_id == id select m;
-
-
 
             var notice_dto = new NoticeDto()
             {
@@ -122,8 +121,8 @@ namespace Board.Controllers
                 UpdateDate = notice.UpdateDate,
                 Views_Number = notice.Views_Number,
 
-                    Comments = comment.ToList(),
-                    
+                Comments = comment.ToList(),
+                FileName = notice.FileName,
             };
             
 
@@ -163,16 +162,26 @@ namespace Board.Controllers
             return View();
         }
 
-
-        // POST: Notices/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,Content,UserName,Category")] Notice notice)
+        public async Task<IActionResult> Create(List<IFormFile> files, [Bind("Id", "Title", "Content", "UserName", "Category")] Notice notice)
         {
-            if (ModelState.IsValid)
+            int result = -1;
+
+            string uploadDir = "D:/code/Board/UploadPath/";
+            try
             {
+                // 업로드 폴더 경로 존재 확인
+                DirectoryInfo di = new DirectoryInfo(uploadDir);
+                // 폴더가 없을 경우 신규 작성
+                if (di.Exists == false) di.Create();
+
+                // 선택한 파일 개수만큼 반복
+                foreach (var formFile in files)
+                {
+                    if (formFile.Length > 0)
+                    {
+                        var fileFullPath = uploadDir + formFile.FileName;
 
                         // 파일명이 이미 존재하는 경우 파일명 변경
                         int filecnt = 1;
@@ -187,6 +196,7 @@ namespace Board.Controllers
 
 
                         notice.fileAttachMent = fileFullPath;
+                        notice.FileName = newFilename;
 
 
                         // 파일 업로드
@@ -210,7 +220,8 @@ namespace Board.Controllers
                 //현재 시간을 데이터 베이스에 넣어준다.
                 DateTime time_now = DateTime.Now;
                 notice.UpdateDate = time_now;
-            
+
+
                 _context.Add(notice);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
@@ -220,24 +231,24 @@ namespace Board.Controllers
 
         // GET: Notices/Edit/5
         public async Task<IActionResult> Edit(int? id)
+    {
+
+
+        if (id == null || _context.Notice == null)
         {
-
-
-            if (id == null || _context.Notice == null)
-            {
-                return NotFound();
-            }
-            Notice notice = new Notice();           
-            notice = _context.Notice.FirstOrDefault(x => x.Id == id);
+            return NotFound();
+        }
+        Notice notice = new Notice();           
+        notice = _context.Notice.FirstOrDefault(x => x.Id == id);
             
            
 
-            if (notice == null)
-            {
-                return NotFound();
-            }
-            return View(notice);
+        if (notice == null)
+        {
+            return NotFound();
         }
+        return View(notice);
+    }
 
         // POST: Notices/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
@@ -316,6 +327,8 @@ namespace Board.Controllers
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+
+
 
         [HttpPost, ActionName("CommentCreate")]
         [ValidateAntiForgeryToken]
@@ -419,7 +432,7 @@ namespace Board.Controllers
             }
             else
             {
-                return RedirectToAction("Details", "Notices", new { id = commentId });
+                return RedirectToAction("Details", "Notices", new { id = notice.Id });
             }
         }
 
--- Board/Models/Notice.cs
+++ Board/Models/Notice.cs
@@ -25,5 +25,6 @@ namespace Board.Models
 
         public byte[]? fileContents { get; set; }
 
+
     }
 }
