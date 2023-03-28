7fdcbc8ff461ff978eecc3c67d327ab8a3c42906
wonchaeYi <wonchae.yi@softpkorea.com>
2023-03-28 12:36:20 +0900

    "게시글 쓰기 구현"

--- Board/Controllers/NoticesController.cs
+++ Board/Controllers/NoticesController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.Encodings.Web;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -28,6 +29,7 @@ namespace Board.Controllers
 {
 
     using Board.Models;
+    using System.Net;
     using System.Reflection.PortableExecutable;
 
     public class NoticeDto
@@ -151,11 +153,15 @@ namespace Board.Controllers
 
             var filemodel = from m in _context.FileModel where m.NoticeId == id select m;
 
+            string content = notice.Content;
+            string encodedContent = WebUtility.HtmlDecode(content);
+
+
             var notice_dto = new NoticeDto()
             {
                 Id = notice.Id,
                 UserName = notice.UserName,
-                Content = notice.Content,
+                Content = encodedContent,
                 Title = notice.Title,
                 UpdateDate = notice.UpdateDate,
                 Views_Number = notice.Views_Number,
@@ -222,9 +228,9 @@ namespace Board.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(List<IFormFile> files, [Bind("Id", "Title", "Content", "UserName", "Category")] Notice notice)
+        public async Task<IActionResult> Create(/*List<IFormFile> files,*/ [Bind("Id", "Title", "Content", "UserName", "Category")] Notice notice)
         {
-            int result = -1;
+            //int result = -1;
 
             if (ModelState.IsValid)
             {
@@ -235,55 +241,55 @@ namespace Board.Controllers
                 _context.Add(notice);
                 await _context.SaveChangesAsync();
 
-                string uploadDir = "D:/code/Board/UploadPath/";
-                try
-                {
-                    // 업로드 폴더 경로 존재 확인
-                    DirectoryInfo di = new DirectoryInfo(uploadDir);
-                    // 폴더가 없을 경우 신규 작성
-                    if (di.Exists == false) di.Create();
+                //string uploadDir = "D:/code/Board/UploadPath/";
+                //try
+                //{
+                //    // 업로드 폴더 경로 존재 확인
+                //    DirectoryInfo di = new DirectoryInfo(uploadDir);
+                //    // 폴더가 없을 경우 신규 작성
+                //    if (di.Exists == false) di.Create();
 
-                    // 선택한 파일 개수만큼 반복
-                    foreach (var formFile in files)
-                    {
-                        if (formFile.Length > 0)
-                        {
-                            var fileFullPath = uploadDir + formFile.FileName;
+                //    // 선택한 파일 개수만큼 반복
+                //    foreach (var formFile in files)
+                //    {
+                //        if (formFile.Length > 0)
+                //        {
+                //            var fileFullPath = uploadDir + formFile.FileName;
 
-                            // 파일명이 이미 존재하는 경우 파일명 변경
-                            int filecnt = 1;
-                            System.String newFilename = string.Empty;
-                            while (new FileInfo(fileFullPath).Exists)
-                            {
-                                var idx = formFile.FileName.LastIndexOf('.');
-                                var tmp = formFile.FileName.Substring(0, idx);
-                                newFilename = tmp + System.String.Format("({0})", filecnt++) + formFile.FileName.Substring(idx);
-                                fileFullPath = uploadDir + newFilename;
-                            }
+                //            // 파일명이 이미 존재하는 경우 파일명 변경
+                //            int filecnt = 1;
+                //            System.String newFilename = string.Empty;
+                //            while (new FileInfo(fileFullPath).Exists)
+                //            {
+                //                var idx = formFile.FileName.LastIndexOf('.');
+                //                var tmp = formFile.FileName.Substring(0, idx);
+                //                newFilename = tmp + System.String.Format("({0})", filecnt++) + formFile.FileName.Substring(idx);
+                //                fileFullPath = uploadDir + newFilename;
+                //            }
 
                            
 
-                            FileModel model = new FileModel();
+                //            FileModel model = new FileModel();
 
-                            model.NoticeId = notice.Id;
-                            model.FilePath = fileFullPath;
-                            model.FileNames = formFile.FileName;
-                            _context.Add(model);
-                            await _context.SaveChangesAsync();
+                //            model.NoticeId = notice.Id;
+                //            model.FilePath = fileFullPath;
+                //            model.FileNames = formFile.FileName;
+                //            _context.Add(model);
+                //            await _context.SaveChangesAsync();
 
-                            // 파일 업로드
-                            using (var stream = new FileStream(fileFullPath, FileMode.CreateNew))
-                            {
-                                await formFile.CopyToAsync(stream);
-                            }
-                        }
-                    }
-                    result = 0;
-                }
-                catch (Exception)
-                {
-                    throw;
-                }
+                //            // 파일 업로드
+                //            using (var stream = new FileStream(fileFullPath, FileMode.CreateNew))
+                //            {
+                //                await formFile.CopyToAsync(stream);
+                //            }
+                //        }
+                    //}
+                //    result = 0;
+                //}
+                //catch (Exception)
+                //{
+                //    throw;
+                //}
                 //Notice notice = new Notice();
                 return RedirectToAction(nameof(Index));
             }
