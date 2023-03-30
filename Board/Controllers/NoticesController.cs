17b3a2e9c353428812fda256ab7ee115bd147312
wonchaeYi <wonchae.yi@softpkorea.com>
2023-03-30 16:42:09 +0900

    에디터적용 후 이미지 업로드

--- Board/Controllers/NoticesController.cs
+++ Board/Controllers/NoticesController.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,13 +23,18 @@ using Microsoft.IdentityModel.Tokens;
 using static Dropbox.Api.Files.ListRevisionsMode;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 using static Dropbox.Api.Sharing.ListFileMembersIndividualResult;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace Board.Controllers
 {
 
     using Board.Models;
+    using Microsoft.EntityFrameworkCore.Metadata.Internal;
+    using System.IO.Pipes;
     using System.Net;
     using System.Reflection.PortableExecutable;
+    using System.IO;
+    using Microsoft.Build.Evaluation;
 
     public class NoticeDto
     {
@@ -64,15 +68,13 @@ namespace Board.Controllers
         //Notice[]?로 해야 Notice의 객체는 게시물에 대한 정보를 담고 있으므로
         //정보를 다 가져올 수 있다..
         public Notice[]? BestNotice { get; set; }
-<<<<<<< HEAD
-        
+
+
         //user의 id
         public int UserId { get; set; }
         public string UserNickName { get; set; }
 
 
-=======
->>>>>>> 689750d4ae2f5ae9cbfd349ac34140be4c3734d3
     }
 
 
@@ -81,10 +83,12 @@ namespace Board.Controllers
 
         //context 
         private readonly BoardContext _context;
+        private IWebHostEnvironment hostEnv;
 
-        public NoticesController(BoardContext context)
+        public NoticesController(BoardContext context, IWebHostEnvironment env)
         {
             _context = context;
+            hostEnv = env;
         }
 
 
@@ -139,7 +143,7 @@ namespace Board.Controllers
             User user = new User();
             var findUser = _context.User.FirstOrDefault(m => m.UserId == id);
 
-            var usernickname="";
+            var usernickname = "";
 
             if (findUser != null)
             {
@@ -150,7 +154,7 @@ namespace Board.Controllers
             {
                 usernickname = "알수없음";
             }
-            
+
 
 
             var NoticeDto = new NoticeDto
@@ -208,17 +212,15 @@ namespace Board.Controllers
         {
 
             Notice notice = new Notice();
-<<<<<<< HEAD
-            notice =  await _context.Notice.FirstOrDefaultAsync(m => m.Id == Id);
-=======
+
             notice = await _context.Notice.FirstOrDefaultAsync(m => m.Id == Id);
+
 
             if (notice.LikeNotice == null)
             {
                 notice.LikeNotice = 0;
             }
 
->>>>>>> 689750d4ae2f5ae9cbfd349ac34140be4c3734d3
             if (notice.LikeNotice == null) {
                 notice.LikeNotice = 0;
             }
@@ -231,42 +233,39 @@ namespace Board.Controllers
 
 
         //파일 다운로드
-<<<<<<< HEAD
+
         //public FileResult FileDownload(string? filename) {
-=======
-        public FileResult FileDownload(string? filename)
-        {
->>>>>>> 689750d4ae2f5ae9cbfd349ac34140be4c3734d3
+
 
         //    FileModel fileModel = new FileModel();
         //    fileModel = _context.FileModel.FirstOrDefault(m => m.FileNames == filename);
 
 
-<<<<<<< HEAD
         //    string filepath = fileModel.FilePath;        // 파일 경로
         //    string filenames = fileModel.FileNames;      // 파일명
         //    string path = filepath;  // 파일 경로 / 파일명
-               
-            
-                
+
+
+
         //    // 파일을 바이트 형식으로 읽음
         //    byte[] bytes = System.IO.File.ReadAllBytes(path);
         //    // 파일 다운로드 처리
         //    return File(bytes, "application/octet-stream", filenames);
         //}
-=======
-            string filepath = fileModel.FilePath;        // 파일 경로
-            string filenames = fileModel.FileNames;      // 파일명
-            string path = filepath;  // 파일 경로 / 파일명
+
+        //string filepath = fileModel.FilePath;        // 파일 경로
+        //string filenames = fileModel.FileNames;      // 파일명
+        //string path = filepath;  // 파일 경로 / 파일명
 
 
 
-            // 파일을 바이트 형식으로 읽음
-            byte[] bytes = System.IO.File.ReadAllBytes(path);
-            // 파일 다운로드 처리
-            return File(bytes, "application/octet-stream", filenames);
-        }
->>>>>>> 689750d4ae2f5ae9cbfd349ac34140be4c3734d3
+        // 파일을 바이트 형식으로 읽음
+        //byte[] bytes = System.IO.File.ReadAllBytes(path);
+        //// 파일 다운로드 처리
+        //return File(bytes, "application/octet-stream", filenames);
+        //}
+
+
 
 
 
@@ -283,99 +282,93 @@ namespace Board.Controllers
             return View();
 
         }
-
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(List<IFormFile> files, [Bind("Id", "Title", "Content", "UserName", "Category")] Notice notice)
+        //[ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create
+            (int? id ,string title, string content, string username, string category)
         {
-
-            int result = -1;
-            if (ModelState.IsValid)
-            {
+            Notice notice = new Notice();
+            if (ModelState.IsValid) {
                 DateTime time_now = DateTime.Now;
                 notice.UpdateDate = time_now;
+                notice.Title = title;
+                notice.Content = content;
+                notice.UserName = username;
+                notice.Category = category;
+
                 _context.Add(notice);
                 await _context.SaveChangesAsync();
-
-                string uploadDir = "D:/code/Board/UploadPath/";
-                try
-                {
-                   // 업로드 폴더 경로 존재 확인
-                   DirectoryInfo di = new DirectoryInfo(uploadDir);
-                    //폴더가 없을 경우 신규 작성
-                            if (di.Exists == false) di.Create();
-
-                    //선택한 파일 개수만큼 반복
-                    foreach (var formFile in files)
-                    {
-                        if (formFile.Length > 0)
-                        {
-                            var fileFullPath = uploadDir + formFile.FileName;
-
-                            //파일명이 이미 존재하는 경우 파일명 변경
-                                    int filecnt = 1;
-                            System.String newFilename = string.Empty;
-                            while (new FileInfo(fileFullPath).Exists)
-                            {
-                                var idx = formFile.FileName.LastIndexOf('.');
-                                var tmp = formFile.FileName.Substring(0, idx);
-                                newFilename = tmp + System.String.Format("({0})", filecnt++) + formFile.FileName.Substring(idx);
-                                fileFullPath = uploadDir + newFilename;
-                            }
-
-<<<<<<< HEAD
-                            FileModel model = new FileModel();
-=======
-
-                           
->>>>>>> 689750d4ae2f5ae9cbfd349ac34140be4c3734d3
-
-                            model.NoticeId = notice.Id;
-                            model.FilePath = fileFullPath;
-                            model.FileNames = formFile.FileName;
-                            _context.Add(model);
-                            await _context.SaveChangesAsync();
-
-                            //파일 업로드
-                            using (var stream = new FileStream(fileFullPath, FileMode.CreateNew))
-                            {
-                             await formFile.CopyToAsync(stream);
-                            }
-                        }
-                    }
-                    result = 0;
-                }
-                catch (Exception)
-                {
-                    throw;
-                }
-
-<<<<<<< HEAD
-                return RedirectToAction(nameof(Index));
+                return Json(new { result = 0 });
             }
-=======
-                //            // 파일 업로드
-                //            using (var stream = new FileStream(fileFullPath, FileMode.CreateNew))
-                //            {
-                //                await formFile.CopyToAsync(stream);
-                //            }
-                //        }
-                    //}
-                //    result = 0;
-                //}
-                //catch (Exception)
-                //{
-                //    throw;
-                //}
-
-                //Notice notice = new Notice();
-                return RedirectToAction(nameof(Index));
-            }
-
-
->>>>>>> 689750d4ae2f5ae9cbfd349ac34140be4c3734d3
             return View(notice);
         }
+
+
+
+
+        //파일 업로드
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+
+            var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+            var uploadPath = Path.Combine(hostEnv.WebRootPath, "Files");
+            var filePath = Path.Combine(uploadPath, fileName);
+
+
+            using (var stream = new FileStream(filePath, FileMode.CreateNew))
+            {
+                await file.CopyToAsync(stream);
+            }
+            return Ok(new { imageUrl = $"/Files/{fileName}" });
+        }
+
+        //try
+        //{
+        //// 업로드 폴더 경로 존재 확인
+        //DirectoryInfo di = new DirectoryInfo(uploadDir);
+
+        //// 폴더가 없을 경우 신규 작성
+        //if (!di.Exists)
+        //{
+        //    di.Create();
+        //}
+
+        // 선택한 파일 개수만큼 반복
+
+        //if (file.Length > 0)
+        //{
+        //    var fileFullPath = Path.Combine(uploadPath, file.FileName);
+
+        //    // 파일명이 이미 존재하는 경우 파일명 변경
+        //    int fileCnt = 1;
+        //    string newFileName = string.Empty;
+        //    while (new FileInfo(fileFullPath).Exists)
+        //    {
+        //        var idx = file.FileName.LastIndexOf('.');
+        //        var tmp = file.FileName.Substring(0, idx);
+        //        newFileName = tmp + string.Format("({0})", fileCnt++) + file.FileName.Substring(idx);
+        //        fileFullPath = Path.Combine(uploadPath, newFileName);
+        //    }
+
+        //    filepath = newFileName;
+
+        // 파일 업로드
+        //using (var stream = new FileStream(fileFullPath, FileMode.CreateNew))
+        //            {
+        //                await file.CopyToAsync(stream);
+        //            }
+        //        }
+
+        //}
+        //catch (Exception)
+        //{
+        //    throw;
+        //}
+
+        //    return Ok(new { imageUrl = $"/uploadpath/{filepath}" });
+        //}
+
         //public async Task<IActionResult> Create(List<IFormFile> files, [Bind("Id", "Title", "Content", "UserName", "Category")] Notice notice)
         //{
         //    int result = -1;
@@ -389,58 +382,58 @@ namespace Board.Controllers
         //        _context.Add(notice);
         //        await _context.SaveChangesAsync();
 
-        //        string uploadDir = "D:/code/Board/UploadPath/";
-        //        try
-        //        {
-        //            업로드 폴더 경로 존재 확인
-        //           DirectoryInfo di = new DirectoryInfo(uploadDir);
-        //            폴더가 없을 경우 신규 작성
-        //            if (di.Exists == false) di.Create();
-
-        //            선택한 파일 개수만큼 반복
-        //            foreach (var formFile in files)
-        //            {
-        //                if (formFile.Length > 0)
+        //        string uploaddir = "d:/code/board/uploadpath/";
+        //                try
         //                {
-        //                    var fileFullPath = uploadDir + formFile.FileName;
+        //                    업로드 폴더 경로 존재 확인
+        //                   directoryinfo di = new directoryinfo(uploaddir);
+        //        폴더가 없을 경우 신규 작성
+        //                    if (di.exists == false) di.create();
 
-        //                    파일명이 이미 존재하는 경우 파일명 변경
-        //                    int filecnt = 1;
-        //                    System.String newFilename = string.Empty;
-        //                    while (new FileInfo(fileFullPath).Exists)
+        //                    선택한 파일 개수만큼 반복
+        //                    foreach (var formfile in files)
         //                    {
-        //                        var idx = formFile.FileName.LastIndexOf('.');
-        //                        var tmp = formFile.FileName.Substring(0, idx);
-        //                        newFilename = tmp + System.String.Format("({0})", filecnt++) + formFile.FileName.Substring(idx);
-        //                        fileFullPath = uploadDir + newFilename;
+        //                        if (formfile.length > 0)
+        //                        {
+        //                            var filefullpath = uploaddir + formfile.filename;
+
+        //        파일명이 이미 존재하는 경우 파일명 변경
+        //                            int filecnt = 1;
+        //        system.string newfilename = string.empty;
+        //                            while (new fileinfo(filefullpath).exists)
+        //                            {
+        //                                var idx = formfile.filename.lastindexof('.');
+        //        var tmp = formfile.filename.substring(0, idx);
+        //        newfilename = tmp + system.string.format("({0})", filecnt++) + formfile.filename.substring(idx);
+        //                                filefullpath = uploaddir + newfilename;
+        //                            }
+
+
+
+        //    filemodel model = new filemodel();
+
+        //    model.noticeid = notice.id;
+        //                            model.filepath = filefullpath;
+        //                            model.filenames = formfile.filename;
+        //                            _context.add(model);
+        //                            await _context.savechangesasync();
+
+        //    파일 업로드
+        //                            using (var stream = new filestream(filefullpath, filemode.createnew))
+        //                            {
+        //                                await formfile.copytoasync(stream);
+        //}
+        //                        }
         //                    }
-
-
-
-        //                    FileModel model = new FileModel();
-
-        //                    model.NoticeId = notice.Id;
-        //                    model.FilePath = fileFullPath;
-        //                    model.FileNames = formFile.FileName;
-        //                    _context.Add(model);
-        //                    await _context.SaveChangesAsync();
-
-        //                    파일 업로드
-        //                    using (var stream = new FileStream(fileFullPath, FileMode.CreateNew))
-        //                    {
-        //                        await formFile.CopyToAsync(stream);
-        //                    }
+        //                    result = 0;
         //                }
+        //                catch (exception)
+        //                {
+        //    throw;
+        //}
+        //notice notice = new notice();
+        //return redirecttoaction(nameof(index));
         //            }
-        //            result = 0;
-        //        }
-        //        catch (Exception)
-        //        {
-        //            throw;
-        //        }
-        //        Notice notice = new Notice();
-        //        return RedirectToAction(nameof(Index));
-        //    }
 
 
         //    return View(notice);
